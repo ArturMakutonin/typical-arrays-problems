03f3214b26a52302bc84e0c5b88583d5887cb23d
Arthur <tntartur@gmail.com>
2021-02-20 16:40:44 +0300

    feat : add 20 true answers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 
 exports.min = function min (array) {
-
   return Math.min.apply(null,array);
 }
 
 
 exports.max = function max (array) {
-        return Math.max.apply(null, array);
-
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  let sum = 0;
-  for (let i = 0 ; i < array.lenth ;i++ ){
-      sum += array[i];
-  }
-    return sum/array.lenth;
+   let sum=0;
+   for (let i=0;i<array.length;i++){
+       sum+=array[i];
+   }
+    return sum / array.length;
 }
